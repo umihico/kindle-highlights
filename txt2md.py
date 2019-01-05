b597b5677e72c2b536e145742e97110b9f4e7f69
umihico <umihico@users.noreply.github.com>
2019-01-05 18:48:15 +0900

    added code quote in main content

--- txt2md.py
+++ txt2md.py
@@ -77,10 +77,12 @@ def _each_txt2md(dirname, filename):
     md_texts.append(
         f"## Last highlight:{date},Total highlights:{len(highlights)}  ")
     tupled_highlights = [x for x in highlights.items() if x[0] != '']
+    md_texts.append('```')
     for page_pos, text in sorted(tupled_highlights, key=lambda x: int(x[0])):
         md_texts.append('  ')
         md_texts.append(f'{page_pos}  ')
         md_texts.append(f'{text}  ')
+    md_texts.append('```')
     md_text = gen_header(
         title + ' by ' + author)
     md_text += '\n'.join(md_texts)
